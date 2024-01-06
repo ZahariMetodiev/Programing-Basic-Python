f7548f4796ba2935a5699f732fb3cbd0f2193f94
ZahariMetodiev <zmetodiev4@gmail.com>
2024-01-06 09:46:55 +0200

    Add task animal_type

---
+++ E03.Conditional Statements Advanced/T03.animal_type.py
@@ -0,0 +1,15 @@
+word = input()
+output = ''
+
+if word == 'dog':
+    output = 'mammal'
+elif word == 'crocodile':
+    output = 'reptile'
+elif word == 'tortoise':
+    output = 'reptile'
+elif word == 'snake':
+    output = 'reptile'
+else:
+    output = 'unknown'
+
+print(output)
